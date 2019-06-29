abad46f2f20d1b1a9376d3ba82d6dba93931567f
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2019-06-29 14:13:15 +0100

    chapter 2 working

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 # decorators modify function that follows it
@@ -6,4 +7,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Averngers movie was so cool!'
+        }
+    ]
+    # using the render_template functino that comes with Jinja2 in Flask
+    # note the template file must be in the ./template directory which is not passed here
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
