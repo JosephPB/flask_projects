893f76358d7f16e7fc1bb87cebda4f468862d6ae
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2019-07-07 20:15:46 +0100

    chapter 8 working - with database remake

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -13,6 +13,13 @@ def load_user(id):
     'The ID passed to the function is going to be a string and so it needs to be converted to an int'
     return User.query.get(int(id))
 
+# to store the relationship between followers and those who are followe, create a table that stores all os the followers
+# this table is not in a model class since it only contains foreign keys
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     '''
     The class inherits from db.Model, which is a base class for all models from Flask-SQLAlchemy. It defines several fields as class variables. Fields are created as instances of the db.Column class, which takes the field type as an argument, plus other optional arguments that, for examples, allowthe indication of which fields are uniques and indexed, which is important so that database searches are effieicnt
@@ -29,7 +36,20 @@ class User(UserMixin, db.Model):
 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
+    
+    followed = db.relationship(
+        # User is the right side entity of the relationship (the left side entity is the parent class). Since this is self-referential, same class if used on both sides
+        # secondary configures association table configured below outside the class
+        'User', secondary=followers,
+        # primaryjoin is the condition that links the left side with the association table
+        # followers.c.follower_id references the follower_id column of the assocation table
+        primaryjoin = (followers.c.follower_id == id),
+        # secondaryjoin indicates the column that links the right side entity
+        secondaryjoin = (followers.c.followed_id == id),
+        # backref defiens how the relationship will be ccessed from the right side entity. From the left side, relationship is named followed, so from the right side using the name followers to represent all the left side users that are linked to the target user in the right side.
+        # in general, lazy = 'dynamic', as in posts, sets up the query to not run until specifically requested
+        backref = db.backref('followers', lazy='dynamic'), lazy = 'dynamic')
+    
     def __repr__(self):
         'Tells Python how to print objects of this class) - useful for debugging'
         return '<User {}>'.format(self.username)
@@ -45,6 +65,31 @@ class User(UserMixin, db.Model):
         # input is an MD5 hash which is generated from lowering the email address, converting it to bytes and then passing it into the hash function
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        'return timestamp ordered posts of those followed'
+        # first, create a database join, merging by the post and followers tables by the condition that the ids of those being followed by a follower matches the id of the writer of the post - this gives a table of all posts, the, who wrote them and then person who followers that person
+        # second, retrieve all the posts where the person who follows that person is self
+        # finally, order by timestamp - using .desc() orders in descending order
+        # note, handling things using database calls like this can be much more efficient than by doing it in individual steps such as: finding all the people one followers, retieveing all their blog posts and then timestamp ordering
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        # also retieve all of self posts for display
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestame.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
