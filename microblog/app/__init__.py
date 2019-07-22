68af22d0351250c3621e9a01fcd276b7f5fd89c7
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2019-07-22 20:56:52 +0100

    chapter 10 under development

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 # name variable is a Python prededined variable set to the name of the module in which it is used
 app = Flask(__name__)
@@ -15,6 +16,7 @@ migrate = Migrate(app, db) # migration engine
 login = LoginManager(app)
 # to be able to give certain pages that he user must login to see, Flask-Login needs to know what the view function is that handles logins - pass the login page to login.login_view
 login.login_view = 'login'
+mail = Mail(app)
 
 # routes module is imported below as it imports from the app variable assigned above
 from app import routes, models, errors
