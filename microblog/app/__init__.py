c172c74a1f2b3e22158fd52f86878f244575b8b7
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2019-11-18 11:33:42 +0000

    chapter 13 under development

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
 
 # name variable is a Python prededined variable set to the name of the module in which it is used
 app = Flask(__name__)
@@ -22,7 +23,10 @@ mail = Mail(app)
 # bootstrap is the flask_bootstap extension that gives a ready to use base template
 # flask is also fully compatible with CSS classes and bootstrap.min.js etc.
 bootstap = Bootstrap(app)
+# moment is calling flask_moment which is a python wrapper around moment.js
 moment = Moment(app)
+#babel is used for enabling translation services easily
+babel = Babel(app)
 
 # routes module is imported below as it imports from the app variable assigned above
 from app import routes, models, errors
@@ -59,3 +63,13 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    '''
+    Provides a high-level interface with the Accept-Language header that clients send with a request
+    Can even give a weighting to language preference, e.g.: Accept-Language: da, en-gb;q=0.8, en;q=0.7
+
+    The use of this function is not yet implemented, please see: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xiii-i18n-and-l10n
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
