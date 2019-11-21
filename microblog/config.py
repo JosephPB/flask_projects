1267b00de227404ab66d775d11820035dbea83b3
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2019-11-21 08:49:31 +0000

    Chapter 20 under development

--- microblog/config.py
+++ microblog/config.py
@@ -4,10 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     '''
     There are various ways of storing config variables, including explicitly defining them as keys in app.config.
-    A way of keeping them separated is to use a Confid class which will then be called and in which all config variables can be stored. You can also have sub-classes in which sub-config variables can be stored
+    A way of keeping them separated is to use a Config class which will then be called and in which all config variables can be stored. You can also have sub-classes in which sub-config variables can be stored
     '''
 
-    # SECRET_KEYS are hard to guess guys which will usually be pased in as environments to protect we forms against Cross0Site Request Forgery
+    # SECRET_KEYS are hard to guess guys which will usually be pased in as environments to protect we forms against Cross-Site Request Forgery
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
